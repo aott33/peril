f40c160f5ca4ebda562977de7e7cfdb41efb281f
aott33 <ott.andrew33@gmail.com>
2026-01-13 00:03:53 -0800

    create dockerfile

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"os/signal"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+)
 
 func main() {
 	fmt.Println("Starting Peril server...")
+
+	connectionString := "amqp://guest:guest@localhost:5672/"
+
+	connection, err := amqp.Dial(connectionString)
+	if err != nil {
+		fmt.Println("Couldn't connect to rabbitmq")
+		return
+	}
+
+	defer connection.Close()
+
+	fmt.Println("Connection Successful!")
+
+	// wait for ctrl+c
+	signalChan := make(chan os.Signal, 1)
+	signal.Notify(signalChan, os.Interrupt)
+	s := <-signalChan
+	fmt.Println("\nShutting down:", s)
 }
